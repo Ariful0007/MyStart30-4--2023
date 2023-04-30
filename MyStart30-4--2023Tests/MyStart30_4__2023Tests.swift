5a2ed32a126f98d80581cbc31551d2c7917925a0
Ariful0007 <arifulpub143@gmail.com>
2023-04-30 08:15:04 +0600

    Initial Commit

---
+++ MyStart30-4--2023Tests/MyStart30_4__2023Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MyStart30_4__2023Tests.swift
+//  MyStart30-4--2023Tests
+//
+//  Created by sang on 9/10/1444 AH.
+//
+
+import XCTest
+@testable import MyStart30_4__2023
+
+class MyStart30_4__2023Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
